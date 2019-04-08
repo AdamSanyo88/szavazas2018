0e53d2c05ce4e089ec12577180676a22c32a2293
Adam Sanyo <adam.sanyo@arm.com>
2019-04-08 11:19:38 +0200

    terkep update test 11

--- js/hu-oevks.js
+++ js/hu-oevks.js
@@ -1,11 +1,9 @@
 ﻿var oevks = {"type":"FeatureCollection","features":[
 {"type":"Feature","id":"100","properties":{"telepules":"Veszprém","tid":"veszprem"},"geometry":{"type":"Polygon","coordinates":[[[17.9093,47.1178],[17.9393,47.1028],[17.9093,47.0878],[17.8793,47.1028],[17.9093,47.1178]]]}},
 
-{"type":"Feature","id":"101","properties":{"telepules":"Pápa","tid":"papa"},"geometry":{"type":"Polygon","coordinates":[[[17.4698,47.341],[17.4998,47.326],[17.4698	47.311],[17.4398,47.326],[17.4698,47.326
-]]]}},
+{"type":"Feature","id":"101","properties":{"telepules":"Pápa","tid":"papa"},"geometry":{"type":"Polygon","coordinates":[[[17.4698,47.341],[17.4998,47.326],[17.4698	47.311],[17.4398,47.326],[17.4698,47.326]]]}},
 
-{"type":"Feature","id":"102","properties":{"telepules":"Ajka","tid":"ajka"},"geometry":{"type":"Polygon","coordinates":[[[17.5518,47.1186],[17.5818,47.1036],[17.5518,47.0886],[17.5218,47.1036],[17.5518,47.1036
-]]]}},
+{"type":"Feature","id":"102","properties":{"telepules":"Ajka","tid":"ajka"},"geometry":{"type":"Polygon","coordinates":[[[17.5518,47.1186],[17.5818,47.1036],[17.5518,47.0886],[17.5218,47.1036],[17.5518,47.1036]]]}},
 
 {"type":"Feature","id":"103","properties":{"telepules":"Balatonfüred","tid":"balatonfured"},"geometry":{"type":"Polygon","coordinates":[[[17.8851,46.9749],[17.9151,46.9599],[17.8851,46.9449],[17.8551,46.9599],[17.8851,46.9599]]]}},
 ]};
